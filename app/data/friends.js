8513f7e1f007045a76e6619992b4e9eedc58b1c9
the 14th web <dennis.itua1@gmail.com>
2019-11-18 22:54:37 -0500

    updating correction to bootstrap link

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Dennis",
+		photo: "https://avatars3.githubusercontent.com/u/52843217?s=460&v=4",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Jeff",
+		photo: "http://www.gstatic.com/tv/thumb/persons/532529/532529_v9_bb.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Bill",
+		photo: "http://www.gstatic.com/tv/thumb/persons/614/614_v9_bc.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Dangote",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Aliko_Dangote.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Mark",
+		photo: "http://www.gstatic.com/tv/thumb/persons/589228/589228_v9_ba.jpg",
 		scores: [4, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
